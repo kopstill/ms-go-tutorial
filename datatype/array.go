78c9339163d3d402f0e72a00dfdcca4ce4615069
kopever <kop.still@gmail.com>
2022-05-27 16:11:15 +0800

    array

---
+++ datatype/array.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [3]int
+	a[1] = 10
+	fmt.Println(a[0])
+	fmt.Println(a[1])
+	fmt.Println(a[len(a)-1])
+
+	// cities := [5]string{"New York", "Paris", "Berlin", "Madrid"}
+	cities := [...]string{"New York", "Paris", "Berlin", "Madrid"}
+	fmt.Println(cities)
+
+	numbers := [...]int{99: -1}
+	fmt.Println("First Position:", numbers[0])
+	fmt.Println("Last Position:", numbers[99])
+	fmt.Println("Length:", len(numbers))
+
+	var twoD [3][5]int
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 5; j++ {
+			twoD[i][j] = (i + 1) * (j + 1)
+		}
+		fmt.Println("Row:", i, ":", twoD[i])
+	}
+	fmt.Println("All at once:", twoD)
+
+	var threeD [3][5][2]int
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 5; j++ {
+			for k := 0; k < 2; k++ {
+				threeD[i][j][k] = (i + 1) * (j + 1) * (k + 1)
+			}
+		}
+	}
+	fmt.Println("All at once:", threeD)
+}
