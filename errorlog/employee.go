f03c6b0e0bc62c679a56559228eab06ef632d998
kopever <kop.still@gmail.com>
2022-06-04 23:34:01 +0800

    common error

--- errorlog/employee.go
+++ errorlog/employee.go
@@ -1,5 +1,11 @@
 package errorlog
 
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
 type Employee struct {
 	ID        int
 	FirstName string
@@ -7,12 +13,26 @@ type Employee struct {
 	Address   string
 }
 
+var ErrNotFound = errors.New("Employee not found")
+
 func getInformation(id int) (*Employee, error) {
-	employee, err := apiCallEmployee(id)
-	return employee, err
+	for tries := 0; tries < 3; tries++ {
+		employee, err := apiCallEmployee(id)
+		if err == nil {
+			return employee, nil
+		}
+		fmt.Println("Server is not responding, retrying ...")
+		time.Sleep(time.Second * 2)
+	}
+
+	return nil, fmt.Errorf("server has failed to respond to get the employee information")
 }
 
 func apiCallEmployee(id int) (*Employee, error) {
+	if id != 1001 {
+		return nil, ErrNotFound
+	}
+
 	employee := Employee{ID: id, LastName: "Doe", FirstName: "John"}
 	return &employee, nil
 }
--- errorlog/errorlog_test.go
+++ errorlog/errorlog_test.go
@@ -1,14 +1,19 @@
 package errorlog
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 )
 
 func TestEmployee(t *testing.T) {
-	employee, err := getInformation(1001)
+	employee, err := getInformation(1000)
 	if err != nil {
-		fmt.Println(err)
+		if errors.Is(err, ErrNotFound) {
+			fmt.Printf("NOT FOUND: %v\n", err)
+		} else {
+			fmt.Println(err)
+		}
 	} else {
 		fmt.Println(employee)
 	}
