81b30749960550b4bfa3cb2ca5dcea8fd244554b
kopever <kop.still@gmail.com>
2022-06-07 16:34:31 +0800

    embedded method

---
+++ method/triangle.go
@@ -0,0 +1,11 @@
+package method
+
+type coloredTriangle struct {
+	// t     triangle
+	triangle
+	color string
+}
+
+// func (ct coloredTriangle) perimeter() int {
+// 	return ct.triangle.size * 10
+// }
---
+++ method/triangle_test.go
@@ -0,0 +1,14 @@
+package method
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestColoredTriangle(t *testing.T) {
+	// triangle := coloredTriangle{triangle: triangle{5}, color: "blue"}
+	triangle := coloredTriangle{triangle{5}, "blue"}
+	// fmt.Println("Perimeter:", triangle.t.perimeter())
+	fmt.Println("Perimeter:", triangle.perimeter())
+	fmt.Println("Color:", triangle.color)
+}
