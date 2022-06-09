9c58bbd65de31b16190bc3a9809f358ce4acba9d
kopever <kop.still@gmail.com>
2022-06-09 17:08:20 +0800

    opt

--- concurrency/foo.go
+++ concurrency/foo.go
@@ -6,27 +6,58 @@ import (
 	"time"
 )
 
+type ApiType string
+
+const (
+	Mazure                 ApiType = "mazure"
+	Azure                  ApiType = "azure"
+	Github                 ApiType = "github"
+	Office                 ApiType = "office"
+	Somewhereintheinternet ApiType = "somewhereintheinternet"
+	Microsoft              ApiType = "microsoft"
+)
+
+type Api struct {
+	apitype ApiType
+	apiurl  string
+}
+
+type ApiResult struct {
+	apitype ApiType
+	result  string
+}
+
 func foo() {
 	start := time.Now()
 
 	// apis := []string{
 	// apis := [6]string{
 	// apis := [10]string{
-	apis := [...]string{
-		"https://management.azure.com",
-		"https://dev.azure.com",
-		"https://api.github.com",
-		"https://outlook.office.com/",
-		"https://api.somewhereintheinternet.com/",
-		"https://graph.microsoft.com",
+	// apis := [...]string{
+	// 	"https://management.azure.com",
+	// 	"https://dev.azure.com",
+	// 	"https://api.github.com",
+	// 	"https://outlook.office.com/",
+	// 	"https://api.somewhereintheinternet.com/",
+	// 	"https://graph.microsoft.com",
+	// }
+
+	apis := []Api{
+		{Mazure, "https://management.azure.com"},
+		{Azure, "https://dev.azure.com"},
+		{Github, "https://api.github.com"},
+		Api{Office, "https://outlook.office.com/"},
+		Api{Somewhereintheinternet, "https://api.somewhereintheinternet.com/"},
+		Api{Microsoft, "https://graph.microsoft.com"},
 	}
 
-	ch := make(chan string)
+	ch := make(chan ApiResult)
 	for _, api := range apis {
-		go checkAPI(api, ch)
+		go checkAPI(api.apitype, api.apiurl, ch)
 	}
 
 	for i := 0; i < len(apis); i++ {
+		// fmt.Printf("%T\n", <-ch)
 		fmt.Println(<-ch)
 	}
 
@@ -38,12 +69,12 @@ func foo() {
 	fmt.Printf("Done! It took %v seconds!\n", elapsed.Seconds())
 }
 
-func checkAPI(api string, ch chan string) {
-	_, err := http.Get(api)
+func checkAPI(apitype ApiType, apiurl string, ch chan ApiResult) {
+	_, err := http.Get(apiurl)
 	if err != nil {
-		ch <- fmt.Sprintf("ERROR: %s is down!", api)
+		ch <- ApiResult{apitype, fmt.Sprintf("ERROR: %s is down!", apiurl)}
 		return
 	}
 
-	ch <- fmt.Sprintf("SUCCESS: %s is up and running!", api)
+	ch <- ApiResult{apitype, fmt.Sprintf("SUCCESS: %s is up and running!", apiurl)}
 }
